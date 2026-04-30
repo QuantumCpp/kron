9b442131894e99d60fffab4486b3e0b9428744b5
NobelC <jeremynobel16@gmail.com>
2026-04-29 23:49:07 -0500

    feat: Comprehensive testing of the tokenization process

--- src/core-system/tokenization.cpp
+++ src/core-system/tokenization.cpp
@@ -6,39 +6,54 @@ std::vector<Token> tokenization(const std::vector<std::string>& arguments){
   std::vector<Token> argument_raw;
   argument_raw.reserve(arguments.size());
   bool only_positional = false;
+  int real_count = 0;
 
-  for(const auto& arg : arguments){
+  for(size_t i = 0 ; i < arguments.size() ; i++){
+
+    if(arguments[i].empty() || arguments[i] == " "){
+      continue;
+    }
+    real_count ++;
+
    //Guardar primer (que no tenga guion) elemento como comando 
-    if(argument_raw.empty() && !arg.starts_with("-") && !only_positional){
-   argument_raw.emplace_back(Token{
+    if(real_count == 1 && !arguments[i].starts_with("-") && !only_positional){
+      argument_raw.emplace_back(Token{
           .type = TypeToken::COMMAND,
-          .name = arg,
+          .name = arguments[i],
           .value = "",
-          });
+      });
       continue;
     }
     
     //Detectar cuando se introdujo el "solo posicinales"
-    if(arg == "--"){
-   only_positional = true;
+    if(arguments[i] == "--"){
+      only_positional = true;
       continue;
     }
-    
+
+    if(arguments[i] == "-"){
+      argument_raw.emplace_back(Token{
+          .type = TypeToken::POSITIONAL,
+          .name = arguments[i],
+          .value = "",
+          });
+    }
+
     //guardar cuando se active solo posicionales 
     if(only_positional){
       argument_raw.emplace_back(Token{
           .type = TypeToken::POSITIONAL,
-          .name = arg,
+          .name = arguments[i],
           .value = "",
           });
       continue;
     }
     
     //guardar opciones cortas | largas | con valor explicito
-    if(arg.starts_with("-")){
+    if(arguments[i].starts_with("-")){
       argument_raw.emplace_back(Token{
           .type = TypeToken::OPTION_NOT_NORMALIZED,
-          .name = arg,
+          .name = arguments[i],
           .value = "",
           });
       continue;
@@ -47,7 +62,7 @@ std::vector<Token> tokenization(const std::vector<std::string>& arguments){
     //Guardar los literales
     argument_raw.emplace_back(Token{
         .type = TypeToken::LITERAL,
-        .name = arg,
+        .name = arguments[i],
         .value = "",
         });
   }
--- tests/TOKENIZATION_TEST.cpp
+++ tests/TOKENIZATION_TEST.cpp
@@ -1,21 +1,104 @@
 #include <gtest/gtest.h>
 #include "../include/core-hpp/tokenization.hpp"
 
-//Verifica que un comando simple se divida correctamente
-TEST(TokenizationSuite, BasicCommands) {
-  std::vector<std::string> input = {"kron", "list", "--all"};
-    auto result = tokenization(input);
+//1. Identificacion del COMMAND 
+//-> El primer token que no comienza con "-" es un comando
+TEST(TokenizationSuite, IdentifyCommand){
+  //Caso 1:
+  std::vector<std::string> input = {"list"};
+  auto result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::COMMAND);
+  //Caso 2:
+  input = {"--all", "list"};
+  result = tokenization(input);
+  for(const auto& test : result){
+    EXPECT_NE(test.type,TypeToken::COMMAND);
+  }
+  //Caso 3:
+  input = {"list", "inspect"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::COMMAND);
+  EXPECT_EQ(result[1].type, TypeToken::LITERAL);
 
-    ASSERT_EQ(result.size(), 3);
-    EXPECT_EQ(result[0].value, "kron");
-    EXPECT_EQ(result[1].value, "list");
+  //Caso 4:
+  input = {};
+  result = tokenization(input);
+  ASSERT_EQ(result.empty(), true);
+
+  //Caso 5:
+  input = {""};
+  result = tokenization(input);
+  ASSERT_EQ(result.empty(), true);
 }
 
-// Test de ejemplo: Verifica el manejo de espacios múltiples
-TEST(TokenizationSuite, ExtraSpaces) {
-  std::vector<std::string> input = {"kron"," ", "","list"};
-    auto result = tokenization(input);
+//Clasificacion de opciones
+TEST(TokenizationSuite, SortOptions){
+  //Caso 1:
+  std::vector<std::string> input = {"--all"};
+  auto result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::OPTION_NOT_NORMALIZED);
+  //Caso 2:
+  input = {"-a"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type,TypeToken::OPTION_NOT_NORMALIZED);
+  //Caso 3:
+  input = {"--sort=option"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::OPTION_NOT_NORMALIZED);
 
-    EXPECT_EQ(result.size(), 2);
-    EXPECT_EQ(result[1].value, "list");
+  //Caso 4:
+  input = {"-"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::POSITIONAL);
+
+  //Cas0 5:
+  input = {"---triple"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::OPTION_NOT_NORMALIZED);
+}
+
+//El token de separacion
+TEST(TokenizationSuite, SeparationToken){
+  //Caso 1:
+  std::vector<std::string> input = {"list", "--", "--all"};
+  auto result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::COMMAND);
+  EXPECT_EQ(result[1].type, TypeToken::POSITIONAL);
+
+  //Caso 2:
+  input = {"list", "--", "src", "--sort"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].type, TypeToken::COMMAND);
+  EXPECT_EQ(result[1].type, TypeToken::POSITIONAL);
+  EXPECT_EQ(result[2].type, TypeToken::POSITIONAL);
+
+  //Caso 3:
+  input = {"--"};
+  result = tokenization(input);
+  EXPECT_EQ(result.empty(), true);
+
+  //Caso 4:
+  input = {"list", "--", "--", "src"};
+  result = tokenization(input);
+  EXPECT_EQ(result[1].type, TypeToken::POSITIONAL);
+}
+
+
+//Strings vacios y espacios
+TEST(TokenizationSuite, EmptyString){
+  //Caso 1:
+  std::vector<std::string> input = {"list", "--all"};
+  auto result = tokenization(input);
+  ASSERT_EQ(result.size() == 2, true);
+
+  //Caso 2:
+  input = {"", "sort"};
+  result = tokenization(input);
+  EXPECT_EQ(result[0].name, "sort");
+  EXPECT_EQ(result[0].type, TypeToken::COMMAND);
+
+  //Caso 3:
+  input = {" "};
+  result = tokenization(input);
+  EXPECT_EQ(result.empty(), true);
 }
